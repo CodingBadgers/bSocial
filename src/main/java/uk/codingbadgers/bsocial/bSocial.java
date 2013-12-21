3109469899901e2b6ebf2a8806d3c5c5d54559a7
James Fitzpatrick <thefishlive@hotmail.com>
2013-12-21 18:59:55 +0000

    Outline of bungee plugin

---
+++ src/main/java/uk/codingbadgers/bsocial/bSocial.java
@@ -0,0 +1,22 @@
+package uk.codingbadgers.bsocial;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class bSocial extends Plugin {
+
+	@Override
+	public void onLoad() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
